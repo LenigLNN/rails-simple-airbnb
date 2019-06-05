22b66cb6d7b66a5f62405d63b33ac6076208bcec
Lena Nigrell <nigrell@live.se>
2019-06-05 08:33:44 +0200

    some more GUI done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning...'
+Flat.destroy_all
+
+puts 'creating 1...'
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+puts 'creating 1...'
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+puts 'creating 2...'
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
+  price_per_night: 110,
+  number_of_guests: 2
+)
+puts 'creating 3...'
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: '1Euston Rd London N1C 4QP',
+  description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+puts 'creating 4...'
+Flat.create!(
+  name: 'Villa Stiro',
+  address: 'Great place to live',
+  description: 'A bit more about this flat...y',
+  price_per_night: 4000,
+  number_of_guests: 10
+)
+puts 'finished!!!'
